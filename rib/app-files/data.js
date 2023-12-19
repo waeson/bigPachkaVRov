3d763a0029f93ffdf4392a671286649b34667cf3
waeson <59735306+waeson@users.noreply.github.com>
2023-12-19 13:35:32 +0700

    Update data.js
    Рыбальное НМ надо убрать: 
    "Разведочная скважина №410р"
    "Кустовая площадка №3"
    "Кустовая площадка №3 с высоты"
    "Кустовая площадка №2"
    "Кустовая площадка №1"

--- rib/app-files/data.js
+++ rib/app-files/data.js
@@ -34,84 +34,15 @@ var APP_DATA = {
           "pitch": -0.05281885509794648,
           "rotation": 0,
           "target": "2---2--"
-        },
-        {
-          "yaw": -1.3215409569892085,
-          "pitch": -0.08692136952306662,
-          "rotation": 0,
-          "target": "4---3--"
-        },
+        },      
         {
           "yaw": -1.5367763660081408,
           "pitch": -0.056188668996490776,
           "rotation": 3.141592653589793,
           "target": "6---4"
-        },
-        {
-          "yaw": 0.0010829900269353487,
-          "pitch": 1.3778413035799968,
-          "rotation": 3.141592653589793,
-          "target": "1---1"
         }
       ],
       "infoHotspots": []
-    },
-    {
-      "id": "1---1",
-      "name": "Кустовая площадка №1",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": -2.5622782604306185,
-        "pitch": 0.5768504272284929,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 3.0766280017321117,
-          "pitch": -0.13034534069416992,
-          "rotation": 0,
-          "target": "2---2--"
-        },
-        {
-          "yaw": 2.7768317163740024,
-          "pitch": -0.16876586114002734,
-          "rotation": 0,
-          "target": "4---3--"
-        },
-        {
-          "yaw": 2.564483076374069,
-          "pitch": -0.08758180666050919,
-          "rotation": 3.141592653589793,
-          "target": "6---4"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": -2.837063609009629,
-          "pitch": 0.2875958586870784,
-          "title": "<span style=\"font-size:14.0pt;mso-bidi-font-size:12.0pt;\nfont-family:&quot;PT Astra Serif&quot;,&quot;serif&quot;;mso-fareast-font-family:&quot;Source Han Sans CN Regular&quot;;\nmso-bidi-font-family:&quot;Lohit Devanagari&quot;;mso-ansi-language:RU;mso-fareast-language:\nRU;mso-bidi-language:RU\">Вахтовый жилой комплекс</span>",
-          "text": "Text"
-        }
-      ]
     },
     {
       "id": "2---2--",
@@ -141,202 +72,16 @@ var APP_DATA = {
         "pitch": 0.718856393028112,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": -1.4017319765466176,
-          "pitch": -0.0183356712061844,
-          "rotation": 0,
-          "target": "4---3--"
-        },
+      "linkHotspots": [        
         {
           "yaw": 2.306624188585854,
           "pitch": 0.004156413326233732,
           "rotation": 0,
           "target": "0---1--"
         },
-        {
-          "yaw": 0.5479546280169458,
-          "pitch": 1.4326132474546238,
-          "rotation": 3.141592653589793,
-          "target": "3---2"
-        },
-        {
+              {
           "yaw": -1.6042515907118648,
           "pitch": -0.026271807892172205,
-          "rotation": 3.141592653589793,
-          "target": "6---4"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3---2",
-      "name": "Кустовая площадка №2",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": -2.156631389213377,
-        "pitch": 0.5731533989146413,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.8038241245615119,
-          "pitch": -0.15043761292444202,
-          "rotation": 6.283185307179586,
-          "target": "4---3--"
-        },
-        {
-          "yaw": 0.39107692972785557,
-          "pitch": -0.09426098339977074,
-          "rotation": 3.141592653589793,
-          "target": "6---4"
-        },
-        {
-          "yaw": -2.135116776479464,
-          "pitch": -0.2756650370053926,
-          "rotation": 0,
-          "target": "0---1--"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "4---3--",
-      "name": "Кустовая площадка №3 с высоты",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 0.35830150757419155,
-        "pitch": 0.7307028940039579,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.338708954041495,
-          "pitch": -0.11671283967462287,
-          "rotation": 0,
-          "target": "0---1--"
-        },
-        {
-          "yaw": -2.6388574369580535,
-          "pitch": -0.05964928880450593,
-          "rotation": 0,
-          "target": "2---2--"
-        },
-        {
-          "yaw": 0.24700265388855058,
-          "pitch": -0.0814085124835735,
-          "rotation": 3.141592653589793,
-          "target": "6---4"
-        },
-        {
-          "yaw": 0.5477901725219105,
-          "pitch": -0.05377143801252515,
-          "rotation": 3.141592653589793,
-          "target": "7---410"
-        },
-        {
-          "yaw": 0.6371058493675186,
-          "pitch": 0.6330252095939031,
-          "rotation": 3.141592653589793,
-          "target": "5---3"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "5---3",
-      "name": "Кустовая площадка №3",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": -2.3462827419422343,
-        "pitch": 0.45504676073249684,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.5584020133614995,
-          "pitch": -0.8907356209742936,
-          "rotation": 0,
-          "target": "4---3--"
-        },
-        {
-          "yaw": -2.227093360634118,
-          "pitch": -0.15844260151871126,
-          "rotation": 0,
-          "target": "0---1--"
-        },
-        {
-          "yaw": -2.57570349495502,
-          "pitch": -0.0790538720524907,
-          "rotation": 0,
-          "target": "2---2--"
-        },
-        {
-          "yaw": 0.6761857287445743,
-          "pitch": -0.08261986396616194,
-          "rotation": 3.141592653589793,
-          "target": "7---410"
-        },
-        {
-          "yaw": 0.39898617151808224,
-          "pitch": -0.10681488618315527,
           "rotation": 3.141592653589793,
           "target": "6---4"
         }
@@ -383,75 +128,7 @@ var APP_DATA = {
           "pitch": -0.14916855494527503,
           "rotation": 0,
           "target": "2---2--"
-        },
-        {
-          "yaw": -2.801957731196767,
-          "pitch": -0.11460838188608946,
-          "rotation": 0,
-          "target": "4---3--"
-        },
-        {
-          "yaw": -3.138599791854654,
-          "pitch": -0.044780776417148616,
-          "rotation": 3.141592653589793,
-          "target": "7---410"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "7---410",
-      "name": "Разведочная скважина №410р",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": -2.956234109749147,
-        "pitch": 1.0513327461479935,
-        "fov": 0.852280833488641
-      },
-      "linkHotspots": [
-        {
-          "yaw": 2.0508727784204446,
-          "pitch": 0.045810657837058955,
-          "rotation": 3.141592653589793,
-          "target": "6---4"
-        },
-        {
-          "yaw": -0.26669642083210476,
-          "pitch": -0.11780113457121999,
-          "rotation": 0,
-          "target": "0---1--"
-        },
-        {
-          "yaw": -0.46600256699701603,
-          "pitch": -0.06865833217117512,
-          "rotation": 0,
-          "target": "2---2--"
-        },
-        {
-          "yaw": -0.6379207997508853,
-          "pitch": -0.039339181868555784,
-          "rotation": 0,
-          "target": "4---3--"
-        }
+        }      
       ],
       "infoHotspots": []
     }
